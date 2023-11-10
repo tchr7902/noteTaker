80bbe48aea37fe9a92b6f3cf8e6bd4d9a31c7589
Trevor Christensen <trevorchristensen3405@gmail.com>
2023-11-09 17:51:20 -0700

    finished get and post requests so they're functional on insomnia

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,36 +1,66 @@
+// bringing in express, fs, path, uuid for ids, starting the app,
+// assigning the port, accessing notes, and accessing the db.json easier
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+const notes = require('../../../db/db.json')
 const app = express();
 const PORT = 3001;
-const dbFilePath = path.join(__dirname, '../../../db/db.json')
+const dbFilePath = path.join(__dirname, '../../../db/db.json');
 
+
+// middleware for the results
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+// access to the public folder
 app.use(express.static('public'));
 
+
+// GET for the landing page, sends the index.html file
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '../../index.html'))
 );
 
+
+// GET for the notes page, sends the notes.html file
 app.get('/notes', (req, res) => 
     res.sendFile(path.join(__dirname, '../../notes.html'))
 )
 
-app.get('/api/notes', (req, res) =>
-    fs.readFile(dbFilePath, 'utf8', (err, data) => {
+
+// GET to read the notes in db.json, jsons the results
+app.get('/api/notes', (req, res) => {
+    return res.json(notes);
+})
+
+
+// POST to create a new note, pushes the note
+app.post('/api/notes', (req, res) => {
+    console.log(`${req.method} request received to add a note!`)
+
+    const newNote = {
+        id: uuidv4(),
+        title: req.body.title,
+        text: req.body.text,
+    };
+
+    notes.push(newNote);
+
+    // writes the db.json file with the new note
+    fs.writeFile(dbFilePath, JSON.stringify(notes), (err) => {
         if(err) {
-            console.error(err);
-        } else {
-            const notes = JSON.parse(data);
-            res.json(notes);
+            console.err(err);
         }
+
+        return res.json(newNote);
     })
-)
+})
 
 
-
+// initializes app
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT}`)
 );
